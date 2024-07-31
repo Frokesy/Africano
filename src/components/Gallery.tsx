5741038dfeaeeaae37ab1e7c3f3b22833a2d134d
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-08-01 00:37:41 +0100

    updated gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -10,26 +10,61 @@ const Gallery = () => {
         Gallery
       </h2>
 
-      <div className="flex w-[90%] overflow-x-auto mx-auto gap-20 mt-20">
+      <div className="flex w-[90%] overflow-x-auto mx-auto gap-20 mt-20 h-[10opx]">
         <img
           src="/assets/gallery/img_one.png"
           alt="img"
-          className="flex-shrink-0 lg:w-[25vw] w-[90%] h-auto"
+          className="flex-shrink-0 lg:w-[30vw] w-[90%] h-[550px]"
         />
         <img
           src="/assets/gallery/img_two.png"
           alt="img"
-          className="flex-shrink-0 lg:w-[25vw] w-[90%] h-auto"
+          className="flex-shrink-0 lg:w-[30vw] w-[90%] h-[550px]"
         />
         <img
           src="/assets/gallery/img_three.png"
           alt="img"
-          className="flex-shrink-0 lg:w-[25vw] w-[90%] h-auto"
+          className="flex-shrink-0 lg:w-[30vw] w-[90%] h-[550px]"
         />
         <img
           src="/assets/gallery/img_four.png"
           alt="img"
-          className="flex-shrink-0 lg:w-[25vw] w-[90%] h-auto"
+          className="flex-shrink-0 lg:w-[30vw] w-[90%] h-[550px]"
+        />
+        <img
+          src="/assets/gallery/img_five.jpg"
+          alt="img"
+          className="flex-shrink-0 lg:w-[30vw] w-[90%] h-[550px]"
+        />
+        <img
+          src="/assets/gallery/img_six.jpg"
+          alt="img"
+          className="flex-shrink-0 lg:w-[30vw] w-[90%] h-[550px]"
+        />
+        <img
+          src="/assets/gallery/img_seven.jpg"
+          alt="img"
+          className="flex-shrink-0 lg:w-[30vw] w-[90%] h-[550px]"
+        />
+        <img
+          src="/assets/gallery/img_eight.jpg"
+          alt="img"
+          className="flex-shrink-0 lg:w-[30vw] w-[90%] h-[550px]"
+        />
+        <img
+          src="/assets/gallery/img_nine.jpg"
+          alt="img"
+          className="flex-shrink-0 lg:w-[30vw] w-[90%] h-[550px]"
+        />
+        <img
+          src="/assets/gallery/img_ten.jpg"
+          alt="img"
+          className="flex-shrink-0 lg:w-[30vw] w-[90%] h-[550px]"
+        />
+        <img
+          src="/assets/gallery/img_eleven.jpg"
+          alt="img"
+          className="flex-shrink-0 lg:w-[30vw] w-[90%] h-[550px]"
         />
       </div>
     </div>
